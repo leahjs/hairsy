a3229bbb91a5a52fffa077cd5b33ef13da66e704
heavenlypeach <mubiynacasey@aol.com>
2015-08-12 18:01:34 -0400

    user migration columns

--- db/migrate/20150812214148_create_users.rb
+++ db/migrate/20150812214148_create_users.rb
@@ -1,7 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :city
+      t.string :country
+      t.string :gender
+      t.string :body_type
       t.timestamps null: false
     end
   end
